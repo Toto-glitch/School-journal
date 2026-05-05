715580f0eb3039c9a04035635d03d6da692333dd
Toto-glitch <sergeevanton03072007@gmail.com>
2026-05-05 14:10:58 +0900

    Try add LoginViewModel

--- SchoolJournal/SchoolJournal/View/Windows/LoginWindow.xaml.cs
+++ SchoolJournal/SchoolJournal/View/Windows/LoginWindow.xaml.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using SchoolJournal.ViewModel;
 
-namespace SchoolJournal.View.Windows
+namespace SchoolJournal
 {
-    /// <summary>
-    /// Логика взаимодействия для LoginWindow.xaml
-    /// </summary>
     public partial class LoginWindow : Window
     {
         public LoginWindow()
         {
             InitializeComponent();
+
+            DataContext = new LoginViewModel(this);
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var viewModel = DataContext as LoginViewModel;
+            viewModel.Password = PasswordBox.Password;
         }
     }
 }
---
+++ SchoolJournal/SchoolJournal/ViewModel/LoginViewModel.cs
@@ -0,0 +1,48 @@
+﻿using SchoolJournal.Helper;
+using System.Windows;
+
+namespace SchoolJournal.ViewModel
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private string _username;
+        private string _password;
+        private string _errorMessage;
+
+        public LoginViewModel(Window win) : base(win) { }
+
+        public string Username
+        {
+            get => _username;
+            set { _username = value; OnPropertyChanged(nameof(Username)); }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(nameof(Password)); }
+        }
+
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set { _errorMessage = value; OnPropertyChanged(nameof(_errorMessage)); }
+        }
+
+        private RelayCommand _loginCommand;
+        public RelayCommand LoginCommand => _loginCommand ?? (new RelayCommand(_ => Login()));
+
+        private void Login()
+        {
+            if (string.IsNullOrWhiteSpace(Username) || string.IsNullOrWhiteSpace(Password))
+            {
+                ErrorMessage = "Заполните пустые поля!";
+                return;
+            }
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            _current_window.Close();
+        }
+    }
+}
